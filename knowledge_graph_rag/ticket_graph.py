9e35514209f1bee3a58fabb8d350d99e7c3b3198
Mostafa Karimzadeh <mos.karimzadeh@gmail.com>
2024-07-08 14:05:55 +0200

    Refactored TicketsGraph.

--- knowledge_graph_rag/ticket_graph.py
+++ knowledge_graph_rag/ticket_graph.py
@@ -1,26 +1,31 @@
-
 import networkx as nx
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 from knowledge_graph_rag.ticket_preprocessing import TextPreprocessor
 
+
 class TicketsGraph:
     def __init__(self, tickets) -> None:
         self.tickets = tickets
         self.preprocessor = TextPreprocessor()
         self.preprocessed_tickets = self.preprocessor.remove_stop_words_from_and_lemmatise_tickets(tickets)
-        self.G = self.create_graph_from_documents()
+        self.G = self.create_graph()
 
-    def create_graph_from_documents(self):
+    def calculate_tfidf_matrix(self):
         vectorizer = TfidfVectorizer()
-        tfidf_matrix = vectorizer.fit_transform(self.preprocessed_tickets)
+        return vectorizer.fit_transform(self.preprocessed_tickets)
 
-        cosine_sim = cosine_similarity(tfidf_matrix)
+    def calculate_cosine_similarity(self, tfidf_matrix):
+        return cosine_similarity(tfidf_matrix)
+
+    def create_graph(self):
+        tfidf_matrix = self.calculate_tfidf_matrix()
+        cosine_sim = self.calculate_cosine_similarity(tfidf_matrix)
 
         G = nx.Graph()
 
-        for i, doc in enumerate(self.preprocessed_tickets):
+        for i, ticket in enumerate(self.preprocessed_tickets):
             G.add_node(i, label=self.tickets[i])
 
         for i in range(len(self.preprocessed_tickets)):
